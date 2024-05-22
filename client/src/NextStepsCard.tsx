10bd780226bed29f8fc4ad058dfe10f75feb41cd
Elliott Hamai <ellhamai@microsoft.com>
2024-05-22 13:35:02 -0700

    Updated next steps card

--- client/src/NextStepsCard.tsx
+++ client/src/NextStepsCard.tsx
@@ -15,7 +15,9 @@ const NextStepsInstructionCard = (props: NextStepsInstructionCardProps) => {
           <h5 className="card-title">Success!</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">{description}</h6>
           <ol>
-            <li><a href={window.location.origin} target="_blank" rel="noreferrer">How to run this app locally using the SWA CLI</a></li>
+          <li><a href='https://go.microsoft.com/fwlink/?linkid=2272184' target="_blank" rel="noreferrer">Learn about how the code in this application works</a></li>
+            <li><a href='https://github.com/azure-template-resources/todo-nodejs-pgsql-googleAuth/blob/main/README.md' target="_blank" rel="noreferrer">How to run this app locally using the SWA CLI</a></li>
+            <li><a href='https://learn.microsoft.com/en-us/azure/app-spaces/overview' target="_blank" rel="noreferrer">App Spaces documentation</a></li>
             <li><a href='https://learn.microsoft.com/en-us/azure/static-web-apps/configuration#routes' target="_blank" rel="noreferrer">How routing works in Static Web Apps</a></li>
             <li><a href='https://learn.microsoft.com/en-us/azure/static-web-apps/authentication-authorization' target="_blank" rel="noreferrer">Authentication and authorization in Static Web Apps</a></li>
           </ol>
